be2eeba6ced0c2eb075403e8ffc60f54b4a8a1f3
pako88 <mail@koenig-pascal.de>
2017-05-27 13:53:50 +0200

    delete file after download

--- app.py
+++ app.py
@@ -3,34 +3,44 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import send_file
+from flask import after_this_request
+
+from datetime import datetime
+import os
 
 import youtube_dl
 
 class MyLogger(object):
     def debug(self, msg):
-        pass
+        print(msg)
 
     def warning(self, msg):
-        pass
+        print(msg)
 
     def error(self, msg):
         print(msg)
 
 def my_hook(d):
     if d['status'] == 'finished':
-        print('Done downloading, now converting ...')
+        print('%s - Done downloading, now converting ...' % datetime.now())
 
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-    'logger': MyLogger(),
-    'progress_hooks': [my_hook],
-    'outtmpl': '%(title)s.%(ext)s',
-}
+convert_to_mp3 = True
+if convert_to_mp3:
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'logger': MyLogger(),
+        'progress_hooks': [my_hook],
+        'outtmpl': '%(title)s.%(ext)s',
+        'restrictfilenames': True,
+    }
+else:
+    pass
+    # todo
 
 app = Flask(__name__)
 
@@ -40,9 +50,21 @@ def index():
         url = request.form['url']
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=False)
+            file_name = ydl.prepare_filename(info_dict)
             ydl.download([url])
-        file_name = '%s.mp3' % info_dict['title']
+        if convert_to_mp3:
+            file_name = '%s.mp3' % os.path.splitext(file_name)[0]
+
+        @after_this_request
+        def remove_file(response):
+            try:
+                os.remove(file_name)
+            except Exception as error:
+                print("Error removing file")
+            return response
+
         return send_file(file_name, as_attachment=True)
+
     elif request.method == 'GET':
         pass
     return render_template('index.html')
