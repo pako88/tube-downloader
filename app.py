67606364a545b0c1ffc125b06cf2df0f1706959e
pako88 <mail@koenig-pascal.de>
2017-05-28 10:52:05 +0200

    add video download

--- app.py
+++ app.py
@@ -24,36 +24,38 @@ def my_hook(d):
     if d['status'] == 'finished':
         print('%s - Done downloading, now converting ...' % datetime.now())
 
-convert_to_mp3 = True
-if convert_to_mp3:
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-        'logger': MyLogger(),
-        'progress_hooks': [my_hook],
-        'outtmpl': '%(title)s.%(ext)s',
-        'restrictfilenames': True,
-    }
-else:
-    pass
-    # todo
-
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         url = request.form['url']
+        audio_only = 'audio_only' in request.form
+
+        ydl_opts = {
+            'logger': MyLogger(),
+            'progress_hooks': [my_hook],
+            'outtmpl': '%(title)s.%(ext)s',
+            'restrictfilenames': True,
+        }
+        if audio_only:
+            ydl_opts['postprocessors'] = [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }]
+            ydl_opts['format'] = 'bestaudio/best'
+        else:
+            ydl_opts['format'] = 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4'
+
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=False)
             file_name = ydl.prepare_filename(info_dict)
             ydl.download([url])
-        if convert_to_mp3:
+        if audio_only:
             file_name = '%s.mp3' % os.path.splitext(file_name)[0]
+        else:
+            file_name = '%s.mp4' % os.path.splitext(file_name)[0]
 
         @after_this_request
         def remove_file(response):
@@ -66,8 +68,7 @@ def index():
         return send_file(file_name, as_attachment=True)
 
     elif request.method == 'GET':
-        pass
-    return render_template('index.html')
+        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=80)
